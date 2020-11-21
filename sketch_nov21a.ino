dc57481cd61213b60e6a419fac5791ddf4c742bd
USER-2U666ARN1Q\Administrator <3166832341@qq.com>
2020-11-21 14:59:45 +0800

    Upload Main Code

---
+++ sketch_nov21a.ino
@@ -0,0 +1,22 @@
+char trainspeed1 = 0;
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F,16,2);
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(19200,SERIAL_8N1);
+  lcd.init();  
+  lcd.backlight();  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    trainspeed1 = Serial.read();
+    lcd.setCursor(0,0);
+    lcd.print("Speed:");
+    lcd.setCursor(8,0);
+    lcd.print(trainspeed1, DEC);
+    delay(500);
+    }
+}
